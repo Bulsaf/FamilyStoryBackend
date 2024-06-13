6b1e99c7ea0e3a13fb9d0943754bc5a1ed7a23f5
Bulat <bulatsafiullin2612@gmail.com>
2024-06-13 07:51:40 +0300

    edit base model + create api_v1

--- cards-application/core/models/base.py
+++ cards-application/core/models/base.py
@@ -1,6 +1,14 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, MappedColumn
+from sqlalchemy.orm import Mapped, mapped_column, declared_attr
+
+from core.config import settings
+
+from utils import camel_case_to_snake_case
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention
+    )
+    id: Mapped[int] = mapped_column(primary_key=True)
     
